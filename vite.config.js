916f28571baae91d969b8d9d8fcd9cfe0e3d54be
i@aarsenev.ru <i@aarsenev.ru>
2025-09-04 15:43:03 +0300

    старница терминала кнопки ввода и формы 2

--- vite.config.js
+++ vite.config.js
@@ -8,14 +8,15 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       manifest: {
-        name: 'HSE Portal',
-        short_name: 'HSE',
+        name: 'IAS Portal',
+        short_name: 'IAS',
         description: 'Личный кабинет по охране труда',
         theme_color: '#ffffff',
         background_color: '#ffffff',
         display: 'standalone',
         orientation: 'portrait',
         start_url: '/',
+        scope: '/',
         icons: [
           {
             src: '/icons/icon-192x192.png',
